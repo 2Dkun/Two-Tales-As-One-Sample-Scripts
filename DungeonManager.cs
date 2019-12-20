e97eaffd0abbc16feee552ebb3b65bc15c9b6769
Hardwin Bui <buihardwin@yahoo.com>
2019-12-19 22:25:17 -0800

    walls are inconsistent

--- DungeonManager.cs
+++ DungeonManager.cs
@@ -8,19 +8,66 @@ public class DungeonManager : MonoBehaviour {
 	public GameObject[] enemies;
 	private FrameCounter frameCounter;
 
+	public GameObject[] groundPoints;
+	private Vector3 playerPos;
+
 	// Use this for initialization
 	void Start () {
 		for(int i = 0; i < enemies.Length; i++){
 			enemies[i].SendMessage("Start");
 		}
+
+		System.Array.Sort(groundPoints, GroundCompare);
+		for(int i = 0; i < 3; i++){
+		//	Debug.Log(groundPoints[i].transform.position.x);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Allow all entities to act
 		for(int i = 0; i < enemies.Length; i++){
 			if(enemies[i])
 				enemies[i].SendMessage("ActFree");
 		}
+		player.GetComponent<Player>().ControlPlayer();
+
+		// Make sure player is in bounds
+		Vector3 curPos = player.transform.position;
+		if(playerPos != curPos){
+			// Find the two points the player is between
+			for(int i = 0; i < groundPoints.Length-1; i++){
+				Vector3 pointA = groundPoints[i].transform.position;
+				Vector3 pointB = groundPoints[i+1].transform.position;
+				if(pointA.x < curPos.x){
+					if(pointB.x >= curPos.x){
+						// Update ground height
+						/*
+						// Dealing with slopes if we want them
+						float newY = curPos.x - pointA.x;
+						newY *= (pointA.y-pointB.y)/(pointA.x-pointB.x);
+						newY += pointA.y;
+						*/
+						// Moved left
+						playerPos.y = player.transform.position.y;
+						if(playerPos.x > curPos.x && pointA.y > curPos.y){
+							player.transform.position = playerPos;
+							Debug.Log("left");
+						}
+						else if(playerPos.x < curPos.x && pointB.y > curPos.y){
+							player.transform.position = playerPos;
+							Debug.Log("right");
+						}
+						else{
+							player.GetComponent<Player>().UpdateGround(pointB.y);
+						}
+					}
+				}
+			}
+			playerPos = player.transform.position;
+		}
+
+
 
 		// Check if enemy has detected player then check if attacked
 			// If attacked: apply hitstun and knockback to player
@@ -43,5 +90,15 @@ public class DungeonManager : MonoBehaviour {
 			player.GetComponent<Player2>().Attacked(1);
 			*/
 
+	}
+
+	// Function used to sort groundPoints by their x position
+	private int GroundCompare(GameObject a, GameObject b) {
+        if (a == null) return (b == null) ? 0 : -1;
+        if (b == null) return 1;
+
+		float xa = a.transform.position.x;
+        float xb = b.transform.position.x;
+        return xa.CompareTo(xb);
 	}
 }
