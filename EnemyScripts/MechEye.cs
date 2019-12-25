8b936a8652520c9d5a72ed21c4b61c7a163fb936
Hardwin Bui <buihardwin@yahoo.com>
2019-12-25 01:02:34 -0800

    Added movement when player isnt detected

--- EnemyScripts/MechEye.cs
+++ EnemyScripts/MechEye.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class MechEye : Enemy {
 
     // Enemy child specific variables
-    
+    private Vector3 oldSpot, newSpot;
+	public float amp;
  
 	// Use this for initialization
 	new void Start () {
 		base.Start();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+		newSpot.x = -(origin.x + foe.walkDist/2);
+		newSpot.y = origin.y + Random.Range(-1, 1);
 	}
 
 	// Allow the enemy to act freely
@@ -28,7 +28,7 @@ public class MechEye : Enemy {
 		switch(curState){
             case States.Undected:	ActIdle();      		break;
 			case States.Grounded:	Agro();        			break;
-			case States.Airborne:    						break;
+			case States.Airborne:   Agro();					break;
 			case States.Attack:  	base.Attack(curAtk); 	break;
             case States.Stunned:	base.Stunned();      	break;
 			case States.KO:			base.DestroyFoe();		break;
@@ -36,11 +36,42 @@ public class MechEye : Enemy {
 		}
 	}
 
- 
-    // Handles AI for enemy when player is not detected
+	// Handles AI for enemy when player is not detected
 	override public void ActIdle() {
+		// Wait a moment before turning around
+		if(transform.localPosition.x > origin.x + foe.walkDist/2){
+			transform.Translate(0, Mathf.Sin(transform.localPosition.x) * Time.deltaTime / amp, 0);
+			if(timer.WaitForXFrames(Random.Range(55,75))){
+				transform.localScale = new Vector2(flipScale, flipScale);
+				transform.localPosition = 
+					new Vector2(origin.x + foe.walkDist/2, transform.localPosition.y);
+				// Find new spot
+				newSpot.x = -(origin.x + foe.walkDist/2);
+				newSpot.y = origin.y + Random.Range(-1, 1);	
+			}
+		}
+		else if(transform.localPosition.x < -(origin.x + foe.walkDist/2)) {
+			transform.Translate(0, Mathf.Sin(transform.localPosition.x) * Time.deltaTime / amp, 0);
+			if(timer.WaitForXFrames(Random.Range(55,75))){
+				transform.localScale = new Vector2(-flipScale, flipScale);
+				transform.localPosition = 
+					new Vector2(-(origin.x + foe.walkDist/2), transform.localPosition.y);
+				// Find new spot
+				newSpot.x = origin.x + foe.walkDist/2;
+				newSpot.y = origin.y + Random.Range(-1, 1);
+			}
+		}
 
-        // IMPLEMENT HOW ENEMY WALKS WHEN PLAYER HASNT BEEN DETECTED
+		// Move to left
+		else if(transform.localScale.x > 0){
+			transform.Translate(-foe.walkSpd * Time.deltaTime, 
+				Mathf.Sin(transform.localPosition.x) * Time.deltaTime / amp, 0);
+		}
+		// Move to right
+		else {
+			transform.Translate(foe.walkSpd * Time.deltaTime, 
+				Mathf.Sin(transform.localPosition.x) * Time.deltaTime / amp, 0);
+		}
 
 		// Try to detect player
 		base.DetectPlayer(true);
