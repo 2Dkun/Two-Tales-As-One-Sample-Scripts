17de7944f761ddb8b2b9385c68e8a2d570c8c398
Hardwin Bui <buihardwin@yahoo.com>
2020-03-10 05:35:54 -0700

    Player+Enemy Refactor

---
+++ BaseEnemy.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemy : Entity {
+
+    [System.Serializable]
+    public class Anims {
+        public SpriteAnimator idle, walk, retreat, stunned;
+    }
+
+    // Base enemy data
+    [Header("Enemy Data")]
+    public Anims anims;
+    public Attack[] attacks;
+    public float moveSpd, moveDist;
+    //public float airAccel, airSpd, jumpHeight, gravity;
+    public float detectRad;
+
+    // Other data
+    protected Vector2 origin;
+    protected BasePlayer player;
+
+    // Initialize data
+    protected new void Start() {
+        base.Start();
+        curState = State.PATROL;
+        origin = transform.localPosition;
+        player = dungeon.GetPlayer();
+    }
+
+    // Apply any additional changes to when enemy is attacked
+    protected override void AttackedChanges() {
+        if (curHP <= 0) {
+            timer.ResetWait();
+            ChangeState(State.KO);
+        }
+    }
+
+    #region Abstract Functions
+    // Allow the entity to act freely
+    public abstract override void ActFreely();
+    // Handles AI for enemy when player is not detected
+    protected abstract void Patrol();
+    // Handles AI for enemy when player is detected
+    protected abstract void Agro();
+    #endregion
+
+    #region Protected Core Functions
+    // See if the player is nearby 
+    protected void DetectPlayer(bool isCircle) {
+        HitBox fov;
+        // Set hitbox for detection
+        if (isCircle)   fov = new HitBox(detectRad, detectRad, -detectRad, -detectRad);
+        else            fov = new HitBox(detectRad, 1, -detectRad, -1);
+        bool isHit = IsHitTarget(fov, gameObject, player.GetHurtBox(), player.gameObject);
+
+        if (isHit) {
+            ChangeState(State.GROUNDED);
+            timer.ResetWait();
+        }
+    }
+
+    // Remove the enemy gameObj upon death
+    protected void DestroyFoe() {
+        // Fade GameObject to nothing
+        float curAlpha = GetComponent<SpriteRenderer>().color.a - Time.deltaTime;
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, curAlpha);
+        if (curAlpha <= 0) { Destroy(gameObject); }
+    }
+
+    // Handles the enemy's state during hitstun
+    protected void ApplyHitstun() {
+        // Return the previous state once out of hitstun
+        if (subTimer.WaitForXFrames(Constants.HURT_TIME)) { 
+            curState = prevState;
+        }
+        // Otherwise play hitstun animation
+        else {
+            if (subTimer.CurFrame() % 3 == 1) {
+                GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
+            }
+            else {
+                GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+            }
+        }
+    }
+    #endregion
+}
---
+++ BasePlayer.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BasePlayer : Entity {
+
+	[System.Serializable]
+	public class Anims {
+		public SpriteAnimator idle, crouch, walk, dash, jump, swap, control;
+		public GameObject ctrlParticles;
+	}
+
+	[System.Serializable]
+	public class PlayerClass {
+		public Anims charAnims;
+		public float walkSpd, dashSpd, airSpd;
+		public float jumpHeight, gravity, fallSpd;
+		public float airAccel, swapRate;
+		public Attack atk, cAtk;
+		public Attack[] skills;
+	}
+
+	[Header("Player Data")]
+	public PlayerClass sword;
+    public PlayerClass shield;
+
+	// Other character data
+	protected PlayerClass curClass;
+	protected float curSP;
+
+    #region Abstract Classes
+    public abstract override void ActFreely();
+
+	#endregion
+
+	#region Virtual Classes
+	protected virtual void StartSwapChanges() { }
+	#endregion
+
+	#region Private Helper Functions
+	// Initiate jump
+	private void Jump() {
+		ChangeState(State.AIRBORNE);
+
+		yVel = Mathf.Abs(curClass.jumpHeight) * 0.2f;
+		if (xVel > 0) xVel = curClass.airSpd;
+		else if (xVel < 0) xVel = -curClass.airSpd;
+	}
+	#endregion
+
+	#region Protected Helper Functions
+	// Attempt to swap to other character
+	protected void AttemptSwap() {
+		if (curSP >= 100) {
+			ChangeState(State.SWAP);
+			timer.ResetWait();
+			StartSwapChanges();
+
+			// Stop enemies from moving
+			dungeon.PauseEnemies(true);
+		}
+		else {
+			// IMPLEMENT STRUGGLE ANIMATION LATER
+
+			dungeon.PauseEnemies(false);
+		}
+	}
+	#endregion
+
+	#region Core Portected Functions
+	protected void MoveGrounded() {
+		// SWAP
+		if (Input.GetKeyDown(KeyCode.W)) {
+			AttemptSwap();
+		}
+
+		// ATTACK
+		else if (Input.GetKeyDown(KeyCode.J)) {
+			ChangeState(State.ATTACK);
+
+			// Decide if the attack was a normal attack or low
+			if (Input.GetKey(KeyCode.S)) curAttack = curClass.cAtk;
+			else curAttack = curClass.atk;
+
+			// Change direction of attack based on input
+			if (Input.GetKey(KeyCode.A)) {
+				transform.localScale = new Vector2(-flipScale, flipScale);
+			}
+			else if (Input.GetKey(KeyCode.D)) {
+				transform.localScale = new Vector2(flipScale, flipScale);
+			}
+
+			timer.ResetWait();
+		}
+
+		// MOVE LEFT
+		else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift)) {
+			GetComponent<SpriteRenderer>().sprite = curClass.charAnims.walk.PlayAnim();
+			transform.localScale = new Vector2(-flipScale, flipScale);
+			transform.Translate(-1 * curClass.walkSpd * Time.deltaTime, 0, 0);
+		}
+		else if (Input.GetKey(KeyCode.A)) {
+			GetComponent<SpriteRenderer>().sprite = curClass.charAnims.dash.PlayAnim();
+			transform.localScale = new Vector2(-flipScale, flipScale);
+			transform.Translate(-1 * curClass.dashSpd * Time.deltaTime, 0, 0);
+		}
+
+		// MOVE RIGHT
+		else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift)) {
+			GetComponent<SpriteRenderer>().sprite = curClass.charAnims.walk.PlayAnim();
+			transform.localScale = new Vector2(flipScale, flipScale);
+			transform.Translate(curClass.walkSpd * Time.deltaTime, 0, 0);
+		}
+		else if (Input.GetKey(KeyCode.D)) {
+			GetComponent<SpriteRenderer>().sprite = curClass.charAnims.dash.PlayAnim();
+			transform.localScale = new Vector2(flipScale, flipScale);
+			transform.Translate(curClass.dashSpd * Time.deltaTime, 0, 0);
+		}
+
+		// DOWN
+		else if (Input.GetKey(KeyCode.S)) {
+			GetComponent<SpriteRenderer>().sprite = curClass.charAnims.crouch.PlayAnim();
+			xVel = 0;
+		}
+
+		// IDLE
+		else {
+			GetComponent<SpriteRenderer>().sprite = curClass.charAnims.idle.PlayAnim();
+			xVel = 0;
+		}
+
+		// Make player jump
+		if (Input.GetKeyDown("space")) {
+			Jump();
+		}
+	}
+
+	// Handles the player's air movement
+	protected void MoveAirborne() {
+		GetComponent<SpriteRenderer>().sprite = curClass.charAnims.jump.PlayAnim();
+
+		// SWAP
+		if (Input.GetKeyDown(KeyCode.W)) {
+			AttemptSwap();
+		}
+
+		// MOVE LEFT
+		if (Input.GetKey(KeyCode.A)) {
+			xVel += -curClass.airAccel * Time.deltaTime * 30;
+			if (xVel < -curClass.airSpd)
+				xVel = -curClass.airSpd;
+		}
+		// MOVE RIGHT
+		else if (Input.GetKey(KeyCode.D)) {
+			this.xVel += curClass.airAccel * Time.deltaTime * 30;
+			if (xVel > curClass.airSpd)
+				xVel = curClass.airSpd;
+		}
+
+		// ATTACK
+		if (Input.GetKeyDown(KeyCode.J)) {
+			ChangeState(State.ATTACK);
+			curAttack = curClass.atk;
+			yVel = 0;
+		}
+		ApplyGravity();
+
+		// Check if player has landed
+		if (transform.localPosition.y <= minHeight) {
+			transform.localPosition = new Vector2(transform.localPosition.x, minHeight);
+			ChangeState(State.GROUNDED);
+			xVel = 0;
+			yVel = 0;
+		}
+	}
+	#endregion
+}
